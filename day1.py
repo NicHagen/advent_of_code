b8ef89f2eadd70ec36a748f18287d18db523b49f
Nicolai Hagen <nicvhagen@gmail.com>
2020-12-02 11:12:53 +0100

    first commit

---
+++ day1.py
@@ -0,0 +1,23 @@
+myarray = [1721, 979, 366, 299, 675, 1456]
+
+def sum2020(n):
+    for i in range(len(n) - 1):
+        for j in range(i + 1, len(n)):
+            if n[i]+n[j] == 2020:
+                return n[i]*n[j]
+
+expenses = []
+with open('day1.txt', 'r') as f:
+    for line in f:
+        expenses.append(int(line.strip()))
+
+# print(sum2020(expenses))
+
+def sum2020part2(n):
+    for i in range(len(n) - 2):
+        for j in range(i + 1, len(n) - 1):
+            for k in range(j + 1, len(n)):
+                if n[i] + n[j] + n[k] == 2020:
+                    return n[i] * n[j] * n[k]
+
+print(sum2020part2(expenses))
---
+++ day2.py
@@ -0,0 +1,31 @@
+mylist = ['1-3 a: abcde', '1-3 c: cdefg', '2-9 c: ccccccccc']
+
+
+
+# count = 0
+# with open('day2.txt', 'r') as f:
+#     for line in f:
+#         chop = line.split()
+#         minmax = chop[0].split('-')
+#         lower = int(minmax[0])
+#         upper = int(minmax[1])
+#         char = chop[1].strip(':')
+#         password = chop[2]
+#         if lower <= password.count(char) <= upper:
+#             count += 1
+
+count = 0
+with open('day2.txt', 'r') as f:
+    for line in f:
+        chop = line.split()
+        minmax = chop[0].split('-')
+        pos1 = int(minmax[0])
+        pos2 = int(minmax[1])
+        char = chop[1].strip(':')
+        password = chop[2]
+        if (password[pos1-1] == char) ^ (password[pos2-1] == char):
+            count += 1
+
+print(count)
+
+
